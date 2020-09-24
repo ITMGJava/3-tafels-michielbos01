c20abc2ee8015434235f714e98bf06d26b0b412d
Michiel-PC <michielbos01@gmail.com>
2020-09-24 12:23:26 +0200

    second commit

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,2 +1,28 @@
 public class Tafels {
+
+    public static void main (String[] args){
+      var x=8;
+      var uitkomst1=x*1;
+        var uitkomst2=x*2;
+        var uitkomst3=x*3;
+        var uitkomst4=x*4;
+        var uitkomst5=x*5;
+        var uitkomst6=x*6;
+        var uitkomst7=x*7;
+        var uitkomst8=x*8;
+        var uitkomst9=x*9;
+        var uitkomst10=x*10;
+
+        System.out.println("Dit is de tafel van " +x);
+        System.out.println(uitkomst1);
+          System.out.println(uitkomst2);
+            System.out.println(uitkomst3);
+                System.out.println(uitkomst4);
+                    System.out.println(uitkomst5);
+                        System.out.println(uitkomst6);
+                            System.out.println(uitkomst7);
+                                System.out.println(uitkomst8);
+                                    System.out.println(uitkomst9);
+                                        System.out.println(uitkomst10);
+    }
 }
