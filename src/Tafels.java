711c28b0b590eb9e589afa65e1ac60da507297bf
Michiel-PC <michielbos01@gmail.com>
2020-09-24 13:45:05 +0200

    third commit

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,7 +1,7 @@
 public class Tafels {
 
     public static void main (String[] args){
-      var x=8;
+      var x=5;
       var uitkomst1=x*1;
         var uitkomst2=x*2;
         var uitkomst3=x*3;
@@ -14,15 +14,15 @@ public class Tafels {
         var uitkomst10=x*10;
 
         System.out.println("Dit is de tafel van " +x);
-        System.out.println(uitkomst1);
-          System.out.println(uitkomst2);
-            System.out.println(uitkomst3);
-                System.out.println(uitkomst4);
-                    System.out.println(uitkomst5);
-                        System.out.println(uitkomst6);
-                            System.out.println(uitkomst7);
-                                System.out.println(uitkomst8);
-                                    System.out.println(uitkomst9);
-                                        System.out.println(uitkomst10);
+        System.out.println("1 x " +x+" = " +uitkomst1);
+          System.out.println("2 x " +x+ " = " +uitkomst2);
+            System.out.println("3 x "+x+ " = "+uitkomst3);
+                System.out.println("4 x "+x+ " = "+uitkomst4);
+                    System.out.println("5 x "+x+ " = "+uitkomst5);
+                        System.out.println("6 x "+x+ " = "+uitkomst6);
+                            System.out.println("7 x "+x+ " = "+uitkomst7);
+                                System.out.println("8 x "+x+ " = "+uitkomst8);
+                                    System.out.println("9 x "+x+ " = "+uitkomst9);
+                                        System.out.println("10 x "+x+ " = "+uitkomst10);
     }
 }
